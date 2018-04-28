c46e10044166fac43443ae51f284c83343903256
Lenilson DeSantis <lenilsonjunior.jr@gmail.com>
2018-04-28 18:21:28 -0300

    :wrench: Devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
   root to: "home#index"
-  devise_for :users
+  devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', registration: 'account', sign_up: 'register' }
 end
